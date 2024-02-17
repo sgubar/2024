d2e5835958f9fb54bfff52af965055e4bf785415
Dusterdin <126003272+Dusterdin@users.noreply.github.com>
2024-02-17 21:55:39 +0200

    Add files via upload

---
+++ DK31/Nazar Humenuk/LB1/math.h
@@ -0,0 +1,18 @@
+
+
+#ifndef math_h
+#define math_h
+
+
+double power(int num, int pow);
+
+double sum_power2(int start , int finish);
+
+double q(int A, int B , int C);
+
+int *get_ABC();
+
+void destroy_mas(int *mas);
+
+
+#endif 
