166bf9b043174ff8862ffd4bec86b8de9124e149
Maria <144729729+maria23923@users.noreply.github.com>
2024-02-18 19:53:09 +0200

    Update main.c

--- DK31/Maria Bogdanova/Homework 1/main.c
+++ DK31/Maria Bogdanova/Homework 1/main.c
@@ -2,8 +2,8 @@
 #include "dk_tool.h"
 
 int main() {
-  int a1, a2, a3, a4, b1, b2, b3, b4;
-  printf("Enter elements for matrix A:\n");
+  int a1, a2, a3, a4, b1, b2, b3, b4; //Оголошуємо змінні матриці
+  printf("Enter elements for matrix A:\n");//виведення на екран
   printf("a1: ");
   scanf("%d", &a1);
   printf("a2: ");
@@ -23,9 +23,16 @@ int main() {
   printf("b4: ");
   scanf("%d", &b4);
 
-  int matrixA[2][2] = {{a1, a2}, {a3, a4}};
-  int matrixB[2][2] = {{b1, b2}, {b3, b4}};
+  int matrixA[2][2] = {{a1, a2}, {a3, a4}}; //задаємо масив для матриці А
+  int matrixB[2][2] = {{b1, b2}, {b3, b4}}; //задаємо масив для матриці В
 
-  multiply_matrices((int *)matrixA, 2, 2, (int *)matrixB, 2, 2); 
+  int *m1 = create_matrix(2, 2);
+  fill_matrix(m1, 2, 2); //заповнення матриць
+  int *m2 = create_matrix(2, 2);
+  fill_matrix(m2, 2, 2);
+  int *result = create_matrix(2, 2);
+  fill_matrix(result, 2, 2);
+ 
+  multiply_matrices((int *)matrixA, 2, 2, (int *)matrixB, 2, 2); //множення матриць
   return 0;
 }
