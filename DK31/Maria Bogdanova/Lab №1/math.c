590fb23457a280215cd6f628d80b8e4f0f14c4fc
Maria <144729729+maria23923@users.noreply.github.com>
2024-02-22 21:09:07 +0200

    Update math.c

--- DK31/Maria Bogdanova/Lab №1/math.c
+++ DK31/Maria Bogdanova/Lab №1/math.c
@@ -1,21 +1,21 @@
 #include "math.h"
 
 #define MOD(n) (n < 0 ? -n : n) // (condition) ? <> : <>
+//Використовуємо тернарний оператор
 
-double power(int base, int pow) {
-
+double power(int base, int pow) // power обчислює base в степені pow
+{
   double result = 1;
-  int pw = MOD(pow);
+  int pw = MOD(pow); //використовується MOD для визначення абсолютного значення pow
   for (int i = 0; i < pw; i ++) {
-    result *= base;
+    result *= base; 
   }
-  return (pow < 0) ? 1.0 / result : result;
+  return (pow < 0) ? 1.0 / result : result; //цикл для множення base на себе pw разів. Якщо pow від'ємне, результат повертається як 1.0 / result
 }
 
 double q(int A, int B, int C, int D) {
   int M=(A*B)-C; 
-    if (M < 0) {
-        // Виведення повідомлення про неправильне число
+    if (M < 0) {  ///Цикл для обчислення модуля
         return (-M)/(power(C, 3))*(A+B+C+D)*power(2, D);
     }else {
       return (M)/(power(C, 3))*(A+B+C+D)*power(2, D);
