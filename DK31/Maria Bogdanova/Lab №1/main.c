57365eed05e212a40bd047736b149b07829d07ed
Maria <144729729+maria23923@users.noreply.github.com>
2024-02-22 21:02:20 +0200

    Update main.c

--- DK31/Maria Bogdanova/Lab №1/main.c
+++ DK31/Maria Bogdanova/Lab №1/main.c
@@ -1,12 +1,8 @@
 #include "math.h"
 #include <stdio.h>
 int main() {
-  // insert code here...
-  printf("Hello, World!\n");
-  int A;
-  int B;
-  int C;
-  int D;
+  printf("Hello, World!\n"); //Виведення повідомлення "Hello world!"
+  int A; int B; int C; int D; //Задаємо дані для входу 
   printf("A: ");
   scanf("%d", &A);
   printf("B: ");
@@ -15,9 +11,9 @@ int main() {
   scanf("%d", &C);
   printf("D: ");
   scanf("%d", &D);
-  printf("2^D: %f\n", power(2, D));
+  printf("2^D: %f\n", power(2, D)); //Використовуємо степеневу функцію
   printf("C^3: %f\n", power(C, 3));
-  printf("The result of calculation Q: %f\n", q(A, B, C, D));
+  printf("The result of calculation Q: %f\n", q(A, B, C, D)); //Вивід результату на екран
 
   return 1;
 }
