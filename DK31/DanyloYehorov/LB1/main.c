2ebf120c61a63e249403a65e3b0e021602b2d810
yehorovd <159530621+yehorovd@users.noreply.github.com>
2024-02-23 10:50:34 +0000

    Update main.c

--- DK31/DanyloYehorov/LB1/main.c
+++ DK31/DanyloYehorov/LB1/main.c
@@ -2,21 +2,14 @@
 #include <stdio.h>
 
 int main(void) {
-    float *a_and_b;
-
-    printf("Enter numbers A and B.\n"
-           "A must be a nonnegative integer.\n"
-           "The mathematical expression\n"
-           "         A * 2 + B = 0\n"
-           "must be false.\n\n");
+    int a;
+    float b;
 
     // read A and B
-    a_and_b = read_AB();
+    read_AB(&a, &b);
 
     // print result
-    printf("A! / |A*2 + B| = %f\n", calc((int) a_and_b[0], a_and_b[1]));
+    printf("A! / |A*2 + B| = %f\n", calc(a, b));
 
-    // clean memory
-    destroy_nums(a_and_b);
     return 0;
 }
