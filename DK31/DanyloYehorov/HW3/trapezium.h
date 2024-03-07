4748f8bad60ceabe4290e54376c64f16ca28cec5
yehorovd <159530621+yehorovd@users.noreply.github.com>
2024-03-07 15:20:26 +0000

    Create trapezium.h

---
+++ DK31/DanyloYehorov/HW3/trapezium.h
@@ -0,0 +1,22 @@
+#ifndef _trapezium_h_
+#define _trapezium_h_
+#include "point.h"
+
+typedef struct tagTrapezium {
+    Point *A;
+    Point *B;
+    Point *C;
+    Point *D;
+} Trapezium;
+
+Trapezium *createTrapeziumFromArray(Point points[]);
+Trapezium *createTrapeziumFromPtrArray(Point *points[]);
+Trapezium *createTrapeziumFromCords(float ax, float ay, float bx, float by, float cx, float cy, float dx, float dy);
+Trapezium * getTrapezium(void);
+
+float areaOfTrapezium(Trapezium *tr);
+void printTrapeziumPoints(Trapezium *tr);
+int validateTrapezium(Trapezium *tr);
+void destroyTrapezium(Trapezium *tr);
+
+#endif
