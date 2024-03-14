8feabe4e9e1cb9a1d8a2c9fe41f296f82e217dae
Maria <144729729+maria23923@users.noreply.github.com>
2024-03-14 22:16:46 +0200

    Update point.c

--- DK31/Maria Bogdanova/Homework 2/point.c
+++ DK31/Maria Bogdanova/Homework 2/point.c
@@ -3,25 +3,25 @@
 #include <stdio.h>
 
 Point *createPoint(int x, int y) {
-    Point *result = (Point *) malloc(sizeof(Point));
+    Point *result = (Point *) malloc(sizeof(Point)); //виділяємо пам'ять для структури
 
-    if (result != NULL) {
-        result->x = x;
-        result->y = y;
+    if (result != NULL) { //перевірка чи result не нуль
+        result->x = x; // це значення буде скопійовано у поле х
+        result->y = y; //це значення буде скопійовано у поле у
     }
     return result;
 }
 
 void destroyPoint(Point *P) {
-    if (P != NULL) {
-        free(P);
+    if (P != NULL) { //перевірка чи Р не нуль
+        free(P); //звільняємо пам'ять 
     }
 }
 
-void printPoint(Point *P) {
-    if (P == NULL) {
+void printPoint(Point *P) { 
+    if (P == NULL) { 
         printf("Invalide reference to point");
         return;
     }
-    printf("(%d, %d)", P->x, P->y);
+    printf("(%d, %d)", P->x, P->y); //виводимо координати
 }
