19046ede1db0ae63cbc459ffe2d398aa1eff02ec
Maria <144729729+maria23923@users.noreply.github.com>
2024-03-14 22:18:28 +0200

    Update main.c

--- DK31/Maria Bogdanova/Homework 2/main.c
+++ DK31/Maria Bogdanova/Homework 2/main.c
@@ -4,22 +4,22 @@
 #define MOD(n) (n < 0 ? -n : n)
 
 int main(void){
-  
+
   printf("Hello, World!\n");
-  
-  Point *A = createPoint(5, 6);
+
+  Point *A = createPoint(3, 5); //точка А
   printPoint(A);
   printf("\n");
 
-  Point *C = {3, 6};
+  Point C = {2, 6}; //точка С
   printPoint(&C);
   printf("\n");
-  
-  Rectangle *R = create_Rectangle(A, C);
-  double area = calculateRectangleArea(R);
-  
-  destroyPoint(A);
-  destroyPoint(C);
 
+  Rectangle *R = create_Rectangle(A, &C); //ствоюємо прямокутник 
+  double area = calculateRectangleArea(R);//обчислюємо площу
+  printf("Площа прямокутника: %.2f\n", MOD(area)); 
+
+  destroyPoint(A); //видаляємо точку А
+//ми не видаляємо точку C, оскільки ми передаємо на неї лише вказівник, а не сам об'єкт
 return 0;
 }
