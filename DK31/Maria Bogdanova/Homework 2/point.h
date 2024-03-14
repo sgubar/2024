23c0e5f16b71df90ec94e4863ccdf12737d49c16
Maria <144729729+maria23923@users.noreply.github.com>
2024-03-14 22:16:22 +0200

    Update point.h

--- DK31/Maria Bogdanova/Homework 2/point.h
+++ DK31/Maria Bogdanova/Homework 2/point.h
@@ -6,10 +6,10 @@
 typedef struct tagPoint {
   int x;
   int y;
-} Point, *PPoint;
+} Point, *PPoint; //Створюємо структуру та вказівник на неї
 
-Point *createPoint(int x, int y);
-void destroyPoint(Point *P);
-void printPoint(Point *P);
+Point *createPoint(int x, int y); //функція для створення точки
+void destroyPoint(Point *P); //функція для видалення точки
+void printPoint(Point *P); //функція для виведення точки
 
-#endif /* point_h */
+#endif
