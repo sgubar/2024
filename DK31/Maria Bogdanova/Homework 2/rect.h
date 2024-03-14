e0c7ad11f5455423e71ae396b246d482b67a700e
Maria <144729729+maria23923@users.noreply.github.com>
2024-03-14 22:17:05 +0200

    Update rect.h

--- DK31/Maria Bogdanova/Homework 2/rect.h
+++ DK31/Maria Bogdanova/Homework 2/rect.h
@@ -2,15 +2,15 @@
 #define _rect_h_
 #include "point.h"
 
-typedef struct tagRectangle {
-    Point *A;
-    Point *C;
+typedef struct tagRectangle { //задаємо структуру Rectangle 
+    Point *A; 
+    Point *C; //задаємо точки А та С
 } Rectangle;
 
 
-Rectangle createRectangle(Point *A, Point *C);
-double calculateRectangleArea(Rectangle *R);
-void destroyRectangle(Rectangle *R);
-void printResult(Rectangle *R);
+Rectangle *create_Rectangle(Point *A, Point *C); //функція для створення прямокутника
+double calculateRectangleArea(Rectangle *R); //функція для обчислення площі
+void destroyRectangle(Rectangle *R);//функція для видалення
+void printResult(Rectangle *R);//функція для виведення результату
 
-#endif
+#endif 
