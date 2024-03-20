6a187061d3700631395fbebfdcd567dbe1d8217e
Makar-zabolotnyi <159521871+Makar-zabolotnyi@users.noreply.github.com>
2024-03-20 22:10:44 +0200

    Update math_tool.h

--- DK31/Zabolotnyi Makar/Lab1/math_tool.h
+++ DK31/Zabolotnyi Makar/Lab1/math_tool.h
@@ -1,5 +1,9 @@
-#ifndef _MATH_TOOL_H_
-#define _MATH_TOOL_H_
+#ifndef math_tool_h
+#define math_tool_h
+
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
 
 int add(int , int );
 int sub(int , int );
@@ -8,6 +12,6 @@ int div(int , int );
 int power(int , int );
 int algebra_sum_power_of_two(int , int );
 int math_modulos(int );
-int check_natural(char a[]);
+int check_natural( );
 
 #endif
