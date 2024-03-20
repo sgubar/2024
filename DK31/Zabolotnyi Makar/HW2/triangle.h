76444c544f7db48c2cbe071658be0031693ddcc7
Makar-zabolotnyi <159521871+Makar-zabolotnyi@users.noreply.github.com>
2024-03-20 22:13:06 +0200

    Update triangle.h

--- DK31/Zabolotnyi Makar/HW2/triangle.h
+++ DK31/Zabolotnyi Makar/HW2/triangle.h
@@ -3,20 +3,18 @@
 
 #include <stdio.h>
 #include "point.h"
-#include "points.h"
 
-
-typedef struct tagTriangle {
-  ArrayOfPoints *points;
+typedef struct tagTriangle{
+  Point *A;
+  Point *B;
+  Point *C;
 } Triangle;
 
 Triangle *createTriangle(int ax, int ay, int bx, int by, int cx, int cy);
-Triangle *createTriangleByPoints(Point *anA, Point *aB, Point *aC);
-Triangle *createEmptyTriangle(void);
+Triangle *createTriangleByPoints(Point *A, Point *B, Point *C);
 
 void destroyTriangle(Triangle *aTriangle);
-
 void printTriangle(Triangle *aTriangle);
-double areaTriangle(Triangle *aTriangle);
+double areaOfTriangle(Triangle *aTriangle);
 
-#endif 
+#endif
