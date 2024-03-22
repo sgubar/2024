a700fb71f22540f544c45e57d80a3107ddfa9cdc
yehorovd <159530621+yehorovd@users.noreply.github.com>
2024-03-22 10:04:38 +0000

    Update list.c

--- DK31/DanyloYehorov/HW4/list.c
+++ DK31/DanyloYehorov/HW4/list.c
@@ -1,10 +1,21 @@
 #include "list.h"
 
-#include <stdlib.h>
+#include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+ListNode *createNode(double number) {
+    // create node
+    ListNode *node = (ListNode *)malloc(sizeof(ListNode));
+    if (node != NULL) {
+        node->number = number;
+        node->next = NULL;
+    }
+    return node;
+}
 
 List *createList(void) {
-    List *result = (List *) malloc(sizeof(List));
+    List *result = (List *)malloc(sizeof(List));
 
     // if List pointer not null setup initial constants
     if (result != NULL) {
@@ -16,26 +27,22 @@ List *createList(void) {
     return result;
 }
 
-
-
 void destroyList(List *list) {
     if (list == NULL) {
-		return ;
-	}
+        return;
+    }
 
-	ListNode *node = list->head;
+    ListNode *node = list->head;
 
     // destroy every node
-	while (NULL != node) {
-		ListNode *next = node->next;
-		free(node);
-		node = next;
-	}
+    while (NULL != node) {
+        ListNode *next = node->next;
+        free(node);
+        node = next;
+    }
 
-	free(list);
+    free(list);
 }
-
-
 
 void insertNumberToList(List *list, unsigned int index, double number) {
     ListNode *previous, *current = list->head;
@@ -45,12 +52,7 @@ void insertNumberToList(List *list, unsigned int index, double number) {
         return;
     }
 
-    // create node
-    ListNode *node = (ListNode *) malloc(sizeof(ListNode));
-    if (node == NULL) {
-        return;
-    }
-    node->number = number;
+    ListNode *node = createNode(number);
 
     // if head element
     if (index == 0) {
@@ -62,48 +64,42 @@ void insertNumberToList(List *list, unsigned int index, double number) {
     }
 
     // cycle through list until index
-	while (current != NULL && i != index) {
+    while (current != NULL && i != index) {
         previous = current;
-		current = current->next;
-		i++;
-	}
+        current = current->next;
+        i++;
+    }
 
-	previous->next = node;
-	node->next = current;
+    previous->next = node;
+    node->next = current;
 
-	list->count++;
+    list->count++;
 }
-
-
 
 void appendNumberToList(List *list, double number) {
-	if (list == NULL) {
-		return;
-	}
+    if (list == NULL) {
+        return;
+    }
 
-	ListNode *node = (ListNode *) malloc(sizeof(ListNode));
-	if (node == NULL) {
-		return;
-	}
+    ListNode *node = createNode(number);
 
-	node->number = number;
-	node->next = NULL;
-
-	if (list->head == NULL) {
+    if (list->head == NULL) {
         // if list empty
-		list->head = list->tail = node;
-	} else {
+        list->head = list->tail = node;
+    } else {
         // if list not empty
-		list->tail->next = node;
-		list->tail = node;
-	}
+        list->tail->next = node;
+        list->tail = node;
+    }
 
-	list->count++;
+    list->count++;
 }
 
-
-
 void deleteNumberFromList(List *list, unsigned int index) {
+    if (list == NULL) {
+        return;
+    }
+
     ListNode *previous, *current = list->head;
     unsigned int i = 0;
 
@@ -122,51 +118,47 @@ void deleteNumberFromList(List *list, unsigned int index) {
     }
 
     // cycle through list until index
-	while (current != NULL && i != index) {
+    while (current != NULL && i != index) {
         previous = current;
-		current = current->next;
-		i++;
-	}
+        current = current->next;
+        i++;
+    }
 
-	previous->next = current->next;
-	free(current);
+    previous->next = current->next;
+    free(current);
 
-	if ((index + 1) == list->count) {
+    if ((index + 1) == listSize(list)) {
         // if last element deleted -> set new tail
         list->tail = previous;
-	}
+    }
 
-	list->count--;
+    list->count--;
 }
-
-
 
 unsigned int listSize(List *list) {
     if (list != NULL) {
         return list->count;
     }
 
-    return -1; // return max possible number
+    return UINT_MAX; // return max possible number
 }
-
-
 
 void printList(List *list) {
     if (list == NULL) {
-		printf("ERROR");
-		return;
-	}
+        printf("[NULL]");
+        return;
+    }
 
-	if (list->head == NULL) {
-		printf("(empty)");
-		return;
-	}
+    if (list->head == NULL) {
+        printf("(empty)");
+        return;
+    }
 
     ListNode *node = list->head->next;
     printf("%lf", list->head->number);
 
     while (node != NULL) {
         printf(" -> %lf", node->number);
-		node = node->next;
-	}
+        node = node->next;
+    }
 }
