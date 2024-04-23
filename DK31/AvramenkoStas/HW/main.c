4ae53c71617ba4554c23eab1188d79c5265081eb
stas1904 <117513993+stas1904@users.noreply.github.com>
2024-04-23 10:13:29 +0300

    Add files via upload

--- DK31/AvramenkoStas/HW/main.c
+++ DK31/AvramenkoStas/HW/main.c
@@ -1,20 +1,46 @@
 #include <stdio.h>
-#include <stdlib.h> // Для використання функції free()
-#include "array.h"
+#include "binary_tree.h"
 
 int main() {
-    int capacity;
-    printf("Enter the capacity of the array: ");
-    scanf("%d", &capacity);
+    struct TreeNode* root = NULL;
+    int value, num_elements;
 
-    struct Array* myArray = create_array(capacity);
+    // Запит користувача про кількість елементів для вставки у дерево
+    printf("Enter the number of elements to insert into the binary tree: ");
+    scanf("%d", &num_elements);
 
-    printf("Array created with capacity %d\n", myArray->capacity);
+    // Вставка елементів у дерево
+    printf("Enter %d elements to insert into the binary tree:\n", num_elements);
+    for (int i = 0; i < num_elements; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &value);
+        root = insert(root, value);
+    }
 
-    // Очищення пам'яті
-    free(myArray->shapes); // Вільна пам'ять, виділена для масиву фігур
-    free(myArray); // Вільна пам'ять, виділена для структури масиву
+    // Виведення дерева
+    printf("Binary Tree Inorder Traversal: ");
+    inorder_traversal(root);
+    printf("\n");
+
+    // Пошук елемента
+    int search_key;
+    printf("Enter the element to search: ");
+    scanf("%d", &search_key);
+    if (search(root, search_key))
+        printf("%d is found in the tree.\n", search_key);
+    else
+        printf("%d is not found in the tree.\n", search_key);
+
+    // Видалення елемента
+    int delete_key;
+    printf("Enter the element to delete: ");
+    scanf("%d", &delete_key);
+    root = deleteNode(root, delete_key);
+    printf("Binary Tree Inorder Traversal after deletion: ");
+    inorder_traversal(root);
+    printf("\n");
 
     return 0;
 }
+
 
