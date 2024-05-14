0f7ff119ec0b906e506df904d634d5a517ad516c
Makar-zabolotnyi <159521871+Makar-zabolotnyi@users.noreply.github.com>
2024-05-14 20:13:11 +0300

    Update main.cpp

--- DK31/Zabolotnyi Makar/Lab6/main.cpp
+++ DK31/Zabolotnyi Makar/Lab6/main.cpp
@@ -1,177 +1,66 @@
-/*
- * GLUT Shapes Demo
- *
- * Written by Nigel Stewart November 2003
- *
- * This program is test harness for the sphere, cone
- * and torus shapes in GLUT.
- *
- * Spinning wireframe and smooth shaded shapes are
- * displayed until the ESC or q key is pressed.  The
- * number of geometry stacks and slices can be adjusted
- * using the + and - keys.
- */
-
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
 #include <GL/glut.h>
 #endif
+#define _USE_MATH_DEFINES
+#include <math.h>
 
-#include <stdlib.h>
-
-static int slices = 16;
-static int stacks = 16;
-
-/* GLUT callback Handlers */
-
-static void resize(int width, int height)
+// Функція, що викликатиметься для малювання сцени
+void display()
 {
-    const float ar = (float) width / (float) height;
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Фон чорний (по завданню білий, але на білому фоні майже нічого не видно)
+    glClear(GL_COLOR_BUFFER_BIT); // Очищення буферу кольору
 
-    glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
+    // Малювання серії відрізків, що є графіком функції
+    glBegin(GL_LINE_STRIP); // Кожна пара вершини задає відрізок
+        glColor3f(0.0f, 0.0f, 1.0f); // Синій колір
+        for (float t = 0; t <= 4*M_PI; t += 0.1) {
+            float x = (2+cos(5*t/2))*cos(t);
+            float y = (2+cos(5*t/2))*sin(t);
+            glVertex2f(x/10, y/10);// Був зробдений масштаб для наглядного розгляднення функцій
+        }
+    glEnd();
 
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity() ;
-}
+    // Малювання координатних осей
+    glBegin(GL_LINE_STRIP);
+        glColor3f(0.0f, 1.0f, 0.0f);// Зелений колір
+        glVertex2f(0.0, -1.0);
+        glVertex2f(0.0, 1.0);
+    glEnd();
+    glBegin(GL_LINE_STRIP);
+        glColor3f(0.0f, 1.0f, 0.0f);// Зелений колір
+        glVertex2f(-1.0, 0.0);
+        glVertex2f(1.0, 0.0);
+    glEnd();
 
-static void display(void)
-{
-    const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-    const double a = t*90.0;
-
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3d(1,0,0);
-
-    glPushMatrix();
-        glTranslated(-2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(-2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glutSwapBuffers();
-}
-
-
-static void key(unsigned char key, int x, int y)
-{
-    switch (key)
-    {
-        case 27 :
-        case 'q':
-            exit(0);
-            break;
-
-        case '+':
-            slices++;
-            stacks++;
-            break;
-
-        case '-':
-            if (slices>3 && stacks>3)
-            {
-                slices--;
-                stacks--;
-            }
-            break;
+    // Малювання позначок на координатних осях
+    for( float x = -1.0; x <= 1.0; x += 0.05){
+        glBegin(GL_LINE_STRIP);
+            glColor3f(0.0f, 1.0f, 0.0f);// Зелений колір
+            glVertex2f(x, -0.03);
+            glVertex2f(x, 0.03);
+        glEnd();
     }
-
-    glutPostRedisplay();
+    for( float y = -1.0; y <= 1.0; y += 0.05){
+        glBegin(GL_LINE_STRIP);
+            glColor3f(0.0f, 1.0f, 0.0f);// Зелений колір
+            glVertex2f(-0.03, y);
+            glVertex2f(0.03, y);
+        glEnd();
+    }
+    glFlush(); // Виконати малювання
 }
 
-static void idle(void)
+
+int main(int argc, char* argv[])
 {
-    glutPostRedisplay();
-}
-
-const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
-const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
-
-const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
-const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
-const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat high_shininess[] = { 100.0f };
-
-/* Program entry point */
-
-int main(int argc, char *argv[])
-{
-    glutInit(&argc, argv);
-    glutInitWindowSize(640,480);
-    glutInitWindowPosition(10,10);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-
-    glutCreateWindow("GLUT Shapes");
-
-    glutReshapeFunc(resize);
-    glutDisplayFunc(display);
-    glutKeyboardFunc(key);
-    glutIdleFunc(idle);
-
-    glClearColor(1,1,1,1);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-
-    glEnable(GL_LIGHT0);
-    glEnable(GL_NORMALIZE);
-    glEnable(GL_COLOR_MATERIAL);
-    glEnable(GL_LIGHTING);
-
-    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
-    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
-
-    glutMainLoop();
-
-    return EXIT_SUCCESS;
+    glutInit(&argc, argv); // Ініціалізація GLUT
+    glutInitWindowSize(1000, 1000); // Ширина та висота вікна
+    glutInitWindowPosition(0, 0); // Положення вікна
+    glutCreateWindow("Dreadnot"); // Створення вікна із заданим ім'ям
+    glutDisplayFunc(display); // Реєстрація функції зворотного
+    // виклику для малювання вікна
+    glutMainLoop(); // Вхід у цикл обробки подій GLUT
+return 0;
 }
