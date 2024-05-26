5a8708d1e2267bc467ef3702804717d54a60367c
Maria <144729729+maria23923@users.noreply.github.com>
2024-05-26 18:50:22 +0300

    Create tree.h

---
+++ DK31/Maria Bogdanova/Homework 5. IntTree/tree.h
@@ -0,0 +1,31 @@
+#ifndef tree_h
+#define tree_h
+
+#include <stdio.h>
+
+typedef struct tagNode //Описуємо вузол дерева, що містить значення та посилання
+                       //на ліве та праве піддерева
+{
+  int value;
+  struct tagNode *leftChild;
+  struct tagNode *rightChild;
+} IntNode;
+
+typedef struct tagTree //Дерево
+{
+  IntNode *root; //посилання на корінь дерева
+  int count; //поточна кількість елементів дерева
+} IntTree;
+
+IntTree *createIntTree();            //створення дерева
+void destroyIntTree(IntTree *aTree); //видалення дерева
+
+void insertIntValueToTree(IntTree *aTree, int aValue); //вставляє елементи в дерево
+IntNode *findNodeWithValue(IntTree *aTree, int aValue); //пошук елемента дерева
+void deleteNodeWithValue(IntTree *aTree, int aValue); //видалення елемента дерева
+
+void printTree(IntTree *aTree); //виведення дерева
+void printNode(IntNode *aNode);
+int countNodesWithTree(IntTree *aTree); //повертає кількість елементів дерева
+
+#endif
