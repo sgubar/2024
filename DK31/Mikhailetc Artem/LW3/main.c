7074b119728099cd82d65b85416d4b31eb07f0cd
artem-mikhaylets <108809724+artem-mikhaylets@users.noreply.github.com>
2024-05-27 13:55:10 +0300

    Update main.c

--- DK31/Mikhailetc Artem/LW3/main.c
+++ DK31/Mikhailetc Artem/LW3/main.c
@@ -1,9 +1,39 @@
 #include "tree.h"
 #include <stdio.h>
 
+// Функція для введення нового значення в дерево
+void insertUserValue(DoubleTree *tree) {
+    double value;
+    printf("Введіть нове значення: ");
+    scanf("%lf", &value);
+    insertDoubleValueToTree(tree, value);
+}
+
+// Функція для видалення значення з дерева
+void deleteUserValue(DoubleTree *tree) {
+    double value;
+    printf("Введіть значення, яке потрібно видалити: ");
+    scanf("%lf", &value);
+    deleteNodeWithValue(tree, value);
+}
+
+// Функція для пошуку значення в дереві
+void searchUserValue(DoubleTree *tree) {
+    double value;
+    printf("Введіть значення, яке потрібно знайти: ");
+    scanf("%lf", &value);
+    DoubleNode *node = findNodeWithValue(tree, value);
+    if (node != NULL) {
+        printf("Значення знайдено: ");
+        printNode(node);
+        printf("\n");
+    } else {
+        printf("Значення не знайдено.\n");
+    }
+}
+
 int main() {
   DoubleTree *tree = createDoubleTree();
-  insertDoubleValueToTree(tree, 53.0); // Root element
   insertDoubleValueToTree(tree, 43.0);
   insertDoubleValueToTree(tree, 65.0);
   insertDoubleValueToTree(tree, 30.0);
@@ -16,21 +46,39 @@ int main() {
   insertDoubleValueToTree(tree, 57.0);
   insertDoubleValueToTree(tree, 87.0);
 
-  printTree(tree);
+  char choice;
+  do {
+      printf("\nВиберіть опцію:\n");
+      printf("1. Ввести нове значення\n");
+      printf("2. Видалити значення\n");
+      printf("3. Знайти значення\n");
+      printf("4. Вивести дерево\n");
+      printf("5. Вийти\n");
 
-  DoubleNode *node = findNodeWithValue(tree, 14.0);
-  if (NULL != node){
-    printNode(node);
-    printf("\n");
-  } else {
-    printf("The node with the key not found\n");
-  }
+      scanf(" %c", &choice);
 
-  deleteNodeWithValue(tree, 30.0);
-  printTree(tree);
+      switch (choice) {
+          case '1':
+              insertUserValue(tree);
+              break;
+          case '2':
+              deleteUserValue(tree);
+              break;
+          case '3':
+              searchUserValue(tree);
+              break;
+          case '4':
+              printTree(tree);
+              break;
+          case '5':
+              printf("До побачення!\n");
+              break;
+          default:
+              printf("Неправильний вибір. Спробуйте ще раз.\n");
+      }
+  } while (choice != '5');
 
   destroyDoubleTree(tree);
 
-  printf("\nHello, World!\n");
   return 0;
 }
