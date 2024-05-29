fce8404622fa00cc02fdb9d336934598cdfa4da8
artem-mikhaylets <108809724+artem-mikhaylets@users.noreply.github.com>
2024-05-29 11:42:16 +0300

    Update main.c

--- DK31/Mikhailetc Artem/HW1/main.c
+++ DK31/Mikhailetc Artem/HW1/main.c
@@ -4,20 +4,19 @@
 int main() {
     char sentence[100];
     char sequence[20];
-    
-    // Введення речення
+
     printf("Введіть речення: ");
     fgets(sentence, sizeof(sentence), stdin);
-    
-    // Введення послідовності
+
+    remove_newline(sentence); 
     printf("Введіть послідовність: ");
     fgets(sequence, sizeof(sequence), stdin);
-    
-    // Підрахунок кількості входжень послідовності в реченні
+
+    remove_newline(sequence);  
+
     int occurrences = count_occurrences(sentence, sequence);
-    
-    // Виведення результату
+
     printf("Кількість входжень послідовності у реченні: %d\n", occurrences);
-    
+
     return 0;
 }
