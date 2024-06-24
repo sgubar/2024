46f3a6ccd2e6fbc6b109a6fd9d2eaa80a052f898
Denys Golub <75337986+Follow1488@users.noreply.github.com>
2024-06-24 22:48:31 +0300

    Add files via upload

---
+++ DK31/Denys Golub/lab2/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "line.h"
+
+int main() {
+    Node *head = NULL;
+
+    int n, data;
+    printf("Enter the number of elements: ");
+    stop_stupid(&n);
+
+    print_elements(&n, &data, &head);
+
+    
+    printf("Iteration:\n");
+    print_list(head);
+    turns_list(&head);
+
+    
+
+    printf("Reversed iteration:\n");
+    print_list(head);
+    turns_list(&head);
+    
+
+    printf("Iteration:\n");
+    print_list(head);
+    turns_list(&head);
+
+    printf("Reversed iteration:\n");
+    print_list(head);
+    delete_list(&head);
+
+
+    return 0;
+}
